549932b34c927f161efeb9063db224d7fd8b2a99
Mark Gallagher <mtgallagher21@gmail.com>
2023-07-05 22:11:38 +0100

    Modified Employee.java

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,43 +1,78 @@
-public class Employee implements Reportable {
+public class Employee  {
 
     private String name;
     private String id;
-    private String details;
+    private Integer age;
+    private String address;
+    private double salary;
+    private String profession;
 
-    public Employee(String name, String id) {
-        setName(name);
-        setId(id);
+    public Employee(String name, String id, Integer age, String address, double salary,String profession) {
+       setName(name);
+       setId(id);
+       setAge(age);
+       setAddress(address);
+       setSalary(salary);
+       setProfession(profession);
     }
 
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getId() {
         return id;
     }
 
-    public void setName(String name) {
-        if(name.length() >= 3 && name.length() <= 24){
-            this.name = name;
-        }
-        else{
-            String message = "Invalid name entered!";
-            throw new IllegalArgumentException(message);
-        }
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public void setId(String id) {
-        if(id.length() >= 8 && id.length() <= 12){
-            this.id = id;
-        }
-        else{
-            String message = "Invalid id entered!";
-            throw new IllegalArgumentException(message);
-        }
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
     }
 
     @Override
-    public void getReportDetails(String details) {
-       
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                ", profession='" + profession + '\'' +
+                '}';
     }
 }
