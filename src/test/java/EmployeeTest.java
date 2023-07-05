bd2f55247c718326f7655b860ea116eb9bbc49b2
Mark Gallagher <mtgallagher21@gmail.com>
2023-07-05 22:07:34 +0100

    Added four arguments to the Employee constructor

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -10,7 +10,7 @@ public class EmployeeTest {
 
     @BeforeEach
     void setUp(){
-        emp = new Employee("Brian Walsh", "412550197");
+        emp = new Employee("Brian Walsh", "412550",38,"Kerry",60000.00, "Construction Worker");
     }
 
     @Test
@@ -22,20 +22,20 @@ public class EmployeeTest {
     @Test
     @DisplayName("Test Id")
     public void testId(){
-        assertEquals("412550197",emp.getId());
+        assertEquals("412550",emp.getId());
     }
 
     @Test
     void testConstructorInvalidName(){
         final String invalid_Title = "Invalid name entered!";
-        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("BBrriiaann WWaallsshh12345","412550197"));
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("BBrriiaann WWaallsshh","412550",38,"Kerry",60000.00, "Construction Worker"));
         assertEquals(invalid_Title,exception.getMessage());
     }
     
     @Test
     void testConstructorInvalidId(){
         final String invalid_Id = "Invalid id entered!";
-        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("Brian Walsh","41255019758637"));
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("Brian Walsh","41255062",38,"Kerry",60000.00, "Construction Worker"));
         assertEquals(invalid_Id,exception.getMessage());
     }
 }
