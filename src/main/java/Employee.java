ad166d2f9a7e80441269e4834b82bcf05c4dd327
mark-gallagher <mtgallagher21@gmail.com>
2023-07-10 22:55:50 +0100

    Modified & Added test methods to Employee.java and EmployeeTest.java

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,19 +1,23 @@
-public class Employee implements Reportable {
+public class Employee  {
 
     private String name;
     private String id;
-    private String details;
+    private Integer age;
+    private String address;
+    private double salary;
+    private String profession;
 
-    public Employee(String name, String id) {
-        setName(name);
-        setId(id);
+    public Employee(String name, String id, Integer age, String address, double salary,String profession) {
+       setName(name);
+       setId(id);
+       setAge(age);
+       setAddress(address);
+       setSalary(salary);
+       setProfession(profession);
     }
 
     public String getName() {
         return name;
-    }
-    public String getId() {
-        return id;
     }
 
     public void setName(String name) {
@@ -26,8 +30,12 @@ public class Employee implements Reportable {
         }
     }
 
+    public String getId() {
+        return id;
+    }
+
     public void setId(String id) {
-        if(id.length() >= 8 && id.length() <= 12){
+        if(id.length() >= 3 && id.length() <= 6){
             this.id = id;
         }
         else{
@@ -36,8 +44,59 @@ public class Employee implements Reportable {
         }
     }
 
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        if(age >= 18 && age <= 40){
+            this.age = age;
+        }else{
+            String message = "Invalid age entered!";
+            throw new IllegalArgumentException(message);
+        }
+
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        if(Character.isUpperCase(address.charAt(0)) && address.length() >= 4){
+            this.address = address;
+        }else{
+            String message = "Invalid address entered!";
+            throw new IllegalArgumentException(message);
+        }
+
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
     @Override
-    public void getReportDetails(String details) {
-       
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                ", profession='" + profession + '\'' +
+                '}';
     }
 }
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -10,7 +10,7 @@ public class EmployeeTest {
 
     @BeforeEach
     void setUp(){
-        emp = new Employee("Brian Walsh", "412550197");
+        emp = new Employee("Brian Walsh", "412550",38,"Kerry",60000.00, "Construction Worker");
     }
 
     @Test
@@ -22,20 +22,46 @@ public class EmployeeTest {
     @Test
     @DisplayName("Test Id")
     public void testId(){
-        assertEquals("412550197",emp.getId());
+        assertEquals("412550",emp.getId());
+    }
+
+    @Test
+    @DisplayName("Test Age")
+    public void testAge(){
+        assertEquals(38,emp.getAge());
+    }
+
+    @Test
+    @DisplayName("Test Address")
+    public void testAddress(){
+        assertEquals("Kerry",emp.getAddress());
     }
 
     @Test
     void testConstructorInvalidName(){
         final String invalid_Title = "Invalid name entered!";
-        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("BBrriiaann WWaallsshh12345","412550197"));
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("BBrriiaann--WWaallsshheeyy","412550",38,"Kerry",60000.00, "Construction Worker"));
         assertEquals(invalid_Title,exception.getMessage());
     }
     
     @Test
     void testConstructorInvalidId(){
         final String invalid_Id = "Invalid id entered!";
-        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("Brian Walsh","41255019758637"));
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("Brian Walsh","41255062",38,"Kerry",60000.00, "Construction Worker"));
         assertEquals(invalid_Id,exception.getMessage());
+    }
+
+    @Test
+    void testConstructorInvalidAge(){
+        final String invalid_Age = "Invalid age entered!";
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("Brian Walsh","412550",42,"Kerry",60000.00, "Construction Worker"));
+        assertEquals(invalid_Age, exception.getMessage());
+    }
+
+    @Test
+    void testConstructorInvalidAddress(){
+        final String invalid_Address = "Invalid address entered!";
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Employee("Brian Walsh","412550",38,"ker",60000.00, "Construction Worker"));
+        assertEquals(invalid_Address, exception.getMessage());
     }
 }
